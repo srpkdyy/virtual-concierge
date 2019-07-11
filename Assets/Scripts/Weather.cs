81910d8e3b3233587eda8283f83ae8776d12eeda
srpkdyy <leader931334@outlook.jp>
2019-07-11 14:06:31 +0900

    Add Weather API

---
+++ Assets/Scripts/Weather.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weather : MonoBehaviour {
+    private IEnumerator GetWeatherInfo(System.Action<string> onFinished)
+    {
+        var request = UnityEngine.Networking.UnityWebRequest.Get("http://weather.livedoor.com/forecast/webservice/json/v1?city=340010");
+        yield return request.SendWebRequest();
+
+        if (request.isHttpError || request.isNetworkError)
+        {
+            Debug.LogErrorFormat("エラー発生で草草の草　→　{0}", request.error);
+            yield break;
+        }
+
+        onFinished(request.downloadHandler.text);
+    }
+
+    private IEnumerator PrintWeatherInfo()
+    {
+        string weatherInfo = null;
+        {
+            System.Action<string> onFinished = (i_text) =>
+            {
+                weatherInfo = i_text;
+            };
+            yield return GetWeatherInfo(onFinished);
+        }
+        Debug.Log(weatherInfo);
+
+        var weatherData = JsonUtility.FromJson<WeatherData>(weatherInfo);
+        SetWeatherData(weatherData);
+    }
+
+  
+    private void SetWeatherData(WeatherData weatherData)
+    {
+        var forecast = weatherData.forecasts[0];
+        Debug.Log(forecast.telop);
+    }
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(PrintWeatherInfo());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/WeatherData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class WeatherData
+{
+    [SerializeField]
+    public ForecastData[] forecasts = null;
+
+
+    [System.Serializable]
+    public class ForecastData
+    {
+        [SerializeField]
+        public string telop = null;
+        [SerializeField]
+        public string date = null;
+        [SerializeField]
+        public TemperatureData temperature = null;
+    }
+
+    [System.Serializable]
+    public class TemperatureData
+    {
+        [SerializeField]
+        public TemperatureDetailData min = null;
+        [SerializeField]
+        public TemperatureDetailData max = null;
+    }
+
+    [System.Serializable]
+    public class TemperatureDetailData
+    {
+        [SerializeField]
+        public string celsius = null;
+    }
+}
